07de5b128137277b6ec8c21162c37b11654d643a
Sarai Soto <sarai.soto@tecsup.edu.pe>
2026-09-02 22:55:50 -0500

    Semana00: agrega Laboratorio 00 - Integracion Xcode con GitHub (version main)

---
+++ Semana00/Lab00/Lab00App.swift
@@ -0,0 +1,17 @@
+//
+//  Lab00App.swift
+//  Lab00
+//
+//  Created by Tecsup on 20/08/26.
+//
+
+import SwiftUI
+
+@main
+struct Lab00App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
