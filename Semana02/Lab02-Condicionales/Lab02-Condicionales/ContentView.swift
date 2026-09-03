c23c330948dafbaea1262c2c593fc54bddd6cecf
Sarai Soto <sarai.soto@tecsup.edu.pe>
2026-09-02 23:50:09 -0500

    feat: Crear proyecto Lab02 y Playground en Semana02

---
+++ Semana02/Lab02-Condicionales/Lab02-Condicionales/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Lab02-Condicionales
+//
+//  Created by Sarai Sthefanie Soto on 2/09/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
